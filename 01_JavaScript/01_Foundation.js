bdbef2be4b13a031ea62a712c564910095b6f2db
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-08-19 12:05:11 +0530

    foundation

---
+++ 01_JavaScript/01_Foundation.js
@@ -0,0 +1,28 @@
+// Jargons, interview questions, and other basics.
+
+
+//* simple primitives:
+//? write a code to greet a user given their first name and last name.
+//?write a code to greet a user based on their gender.{if else}
+//?write a code that counts from 0 to 1000 and print (for loop.)
+
+// let, var = original way to define a variable, const
+
+//*can be redeclare and rewrite:
+var a=1;
+a=2;
+var a = 3;
+console.log(a)
+
+
+// * can't be redeclare and can be rewrite:
+let b=1;
+b=2;
+console.log(b)
+
+
+//* complex primitives:
+//? write a code that prints even number in the array.
+//? write a code that prints largest number in the array.
+//?write a code that prints all the male people's first name given a complex object.
+//?write a code that reverses all the elements of the array.
--- test.js
+++ test.js
@@ -1,1 +1,3 @@
 console.log("jai shree ji Krishna")
+console.log("Hello, world!");
+console.log(a);
