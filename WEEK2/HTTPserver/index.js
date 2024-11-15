de1c5e4fdf34c777f228f717ba366e0e229cfcb1
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-11-15 17:20:46 +0530

    incomplete express and HTTP video

--- WEEK2/HTTPserver/index.js
+++ WEEK2/HTTPserver/index.js
@@ -1,1 +1,19 @@
-const express = require('express'); //importing very famous library  "Express"
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/routes', (req, res) => {
+    res.json({
+        name: "Harshita",
+        age: 21,
+        city: "Pune"
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello, world! it's me Harshita");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
